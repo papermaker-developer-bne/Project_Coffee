10937cc5f0176413a9480d49923ae945d9e630b9
Ike <ike.qian42@gmail.com>
2020-02-29 12:44:12 +1100

    Move JSON reader to JsonHelper.cs to share.

--- Coffee.API/Controllers/DrinkController.cs
+++ Coffee.API/Controllers/DrinkController.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 using System.IO;
 using Coffee.API.Models;
+using Coffee.API.Processor;
 
 namespace Coffee.API.Controllers
 {
@@ -19,105 +20,6 @@ namespace Coffee.API.Controllers
     [Route("[controller]")]
     public class DrinkController : ControllerBase
     {
-
-        public class JsonHelper
-        {
-            private string _jsonName;
-            private string _path;
-            private IConfiguration Configuration { get; set; }
-            public JsonHelper(string jsonName)
-            {
-                _jsonName = jsonName;
-                if (!jsonName.EndsWith(".json"))
-                    _path = $"{jsonName}.json";
-                else
-                    _path = jsonName;
-                //ReloadOnChange = true. Reload when JSON file is modified
-                Configuration = new ConfigurationBuilder()
-                .Add(new JsonConfigurationSource { Path = _path, ReloadOnChange = true, Optional = true })
-                .Build();
-            }
-
-            /// <summary>
-            /// Read JSON and return objects
-            /// </summary>
-            /// <returns></returns>
-            public T Read<T>() => Read<T>("");
-
-            /// <summary>
-            /// Read JSON and return objects based on section
-            /// </summary>
-            /// <returns></returns>
-            public T Read<T>(string section)
-            {
-                try
-                {
-                    using (var file = new StreamReader(_path))
-                    using (var reader = new JsonTextReader(file))
-                    {
-                        var jObj = (JArray)JToken.ReadFrom(reader);
-                        if (!string.IsNullOrWhiteSpace(section))
-                        {
-                            var secJt = jObj[section];
-                            if (secJt != null)
-                            {
-                                return JsonConvert.DeserializeObject<T>(secJt.ToString());
-                            }
-                        }
-                        else
-                        {
-                            return JsonConvert.DeserializeObject<T>(jObj.ToString());
-                        }
-                    }
-                }
-                catch (System.Exception ex)
-                {
-                    throw ex;
-                }
-                return default(T);
-            }
-
-            /// <summary>
-            /// Read from JSON and return list
-            /// </summary>
-            /// <returns></returns>
-            public List<T> ReadList<T>() => ReadList<T>("");
-
-            /// <summary>
-            /// Read from JSON and return list based on section
-            /// </summary>
-            /// <returns></returns>
-            public List<T> ReadList<T>(string section)
-            {
-                try
-                {
-                    using (var file = new StreamReader(_path))
-                    using (var reader = new JsonTextReader(file))
-                    {
-                        var jObj = (JObject)JToken.ReadFrom(reader);
-                        if (!string.IsNullOrWhiteSpace(section))
-                        {
-                            var secJt = jObj[section];
-                            if (secJt != null)
-                            {
-                                return JsonConvert.DeserializeObject<List<T>>(secJt.ToString());
-                            }
-                        }
-                        else
-                        {
-                            return JsonConvert.DeserializeObject<List<T>>(jObj.ToString());
-                        }
-                    }
-                }
-                catch (System.Exception ex)
-                {
-                    throw ex;
-                }
-                return default(List<T>);
-            }
-        
-        }
-
         public class DrinkProvider
         {
 
---
+++ Coffee.API/Processor/JsonHelper.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Coffee.API.Processor
+{
+    public class JsonHelper
+    {
+        private string _jsonName;
+        private string _path;
+        private IConfiguration Configuration { get; set; }
+        public JsonHelper(string jsonName)
+        {
+            _jsonName = jsonName;
+            if (!jsonName.EndsWith(".json"))
+                _path = $"{jsonName}.json";
+            else
+                _path = jsonName;
+            //ReloadOnChange = true. Reload when JSON file is modified
+            Configuration = new ConfigurationBuilder()
+            .Add(new JsonConfigurationSource { Path = _path, ReloadOnChange = true, Optional = true })
+            .Build();
+        }
+
+        /// <summary>
+        /// Read JSON and return objects
+        /// </summary>
+        /// <returns></returns>
+        public T Read<T>() => Read<T>("");
+
+        /// <summary>
+        /// Read JSON and return objects based on section
+        /// </summary>
+        /// <returns></returns>
+        public T Read<T>(string section)
+        {
+            try
+            {
+                using (var file = new StreamReader(_path))
+                using (var reader = new JsonTextReader(file))
+                {
+                    var jObj = (JArray)JToken.ReadFrom(reader);
+                    if (!string.IsNullOrWhiteSpace(section))
+                    {
+                        var secJt = jObj[section];
+                        if (secJt != null)
+                        {
+                            return JsonConvert.DeserializeObject<T>(secJt.ToString());
+                        }
+                    }
+                    else
+                    {
+                        return JsonConvert.DeserializeObject<T>(jObj.ToString());
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Read from JSON and return list
+        /// </summary>
+        /// <returns></returns>
+        public List<T> ReadList<T>() => ReadList<T>("");
+
+        /// <summary>
+        /// Read from JSON and return list based on section
+        /// </summary>
+        /// <returns></returns>
+        public List<T> ReadList<T>(string section)
+        {
+            try
+            {
+                using (var file = new StreamReader(_path))
+                using (var reader = new JsonTextReader(file))
+                {
+                    var jObj = (JObject)JToken.ReadFrom(reader);
+                    if (!string.IsNullOrWhiteSpace(section))
+                    {
+                        var secJt = jObj[section];
+                        if (secJt != null)
+                        {
+                            return JsonConvert.DeserializeObject<List<T>>(secJt.ToString());
+                        }
+                    }
+                    else
+                    {
+                        return JsonConvert.DeserializeObject<List<T>>(jObj.ToString());
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            return default(List<T>);
+        }
+
+    }
+}
